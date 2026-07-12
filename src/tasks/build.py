81e1ff2fa1cd3fa37271d624ae8a4347e3aa0d45
q4niel <git.unrigged621@passmail.com>
2026-07-12 02:27:12 +0200

    add basic c++ compilation and linking to maul 'build' task

--- src/tasks/build.py
+++ src/tasks/build.py
@@ -1,19 +1,48 @@
+import os
+import shutil
 import util
-from util import Config as c
+from util import Config as cfg
+import subprocess
 
 def task() -> None:
-    if not c.isValid:
-        print("Config isn't valid!")
+    if not cfg.isValid:
+        print(util.strColour(util.Colour.Red, "| Error: "), end="")
+        print("Invalid Config File!")
+        return
 
-    print(f"Extensions: {c.hxxExtension}, {c.cxxExtension}")
+    if not os.path.exists(maulDir:= "mnt/.maul"):
+        print(util.strColour(util.Colour.Red, "| Error: "), end="")
+        print("Directory", util.strColour(util.Colour.Cyan, maulDir), "does not exist!")
+        print(util.strColour(util.Colour.Red, "| "), end="")
+        print("Have the", util.strColour(util.Colour.Cyan, "setup"), "task been ran?")
+        return
 
-    for bin in c.binaries:
-        print(f"Name: {bin.name}")
-        print(f"Filename: {bin.filename}")
-        print(f"Type: {bin.type}")
-        print(f"SrcDir: {bin.sourceDirectory}")
-        print("Sources:")
-        for src in bin.sources:
-            print(f"  {src}")
-        print("__________\n")
+    if os.path.exists(workerDir:= f"{maulDir}/worker"):
+        shutil.rmtree(workerDir)
+    os.mkdir(workerDir)
+
+    for bin in cfg.binaries:
+        buildBin(bin, workerDir)
+
+    shutil.rmtree(workerDir)
 #task()
+
+def buildBin(bin: util.Binary, workerDir: str) -> None:
+    for src in bin.sources:
+        subprocess.run ([
+            "clang++",
+            "-std=c++23",
+            "-c",
+            f"mnt/{bin.sourceDirectory}/{src}.{cfg.cxxExtension}",
+            "-o",
+            f"{workerDir}/{f"{bin.sourceDirectory}/{src}".replace("/", "__")}.o"
+        ])
+
+    subprocess.run ([
+        "clang++",
+        "-std=c++23",
+        *(f"{workerDir}/{o}" for o in os.listdir(workerDir)),
+        "-o",
+        f"mnt/.maul/out/{bin.filename}"
+    ])
+#buildBin()
