66531f463faa850b2deddc84262dfd3308a056c6
q4niel <git.unrigged621@passmail.com>
2026-08-09 13:48:57 +0200

    rename Maul task 'setup' to 'task-gen'

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,7 +1,7 @@
 from typing import Dict, Callable
-from . import setup, build#noqa
+from . import task_gen, build#noqa
 
 dict: Dict[str, Callable[[], None]] = {
     "build": lambda: build.task(),
-    "setup": lambda: setup.task()
+    "task-gen": lambda: task_gen.task()
 }
