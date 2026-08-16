3e773c118c5c46c91318225a1d30e2c199b563ef
q4niel <git.unrigged621@passmail.com>
2026-08-17 00:48:33 +0200

    c and cxx greeting tests now print command line arguments

--- tst/hello_c/src/main.c
+++ tst/hello_c/src/main.c
@@ -1,11 +1,17 @@
+#include <stdio.h>
+
 #ifdef DEBUG
-    #include <stdio.h>
     #define PRINT(...) printf(__VA_ARGS__);
 #else
     #define PRINT(...)
 #endif
 
 int main(int argc, char **argv) {
-    PRINT("hello c world\n")
+    PRINT("hello c world\n\n")
+
+    for (int i = 0; i < argc; i++) {
+        printf("Argument %i: %s\n", i, argv[i]);
+    }
+
     return 0;
 }
--- tst/hello_cxx/src/main.cxx
+++ tst/hello_cxx/src/main.cxx
@@ -9,6 +9,11 @@ inline constexpr bool kDebug =
 
 auto main(int argc, char **argv) -> int {
     if constexpr (kDebug)
-        std::println("hello c++ world");
+        std::println("hello c++ world\n");
+
+    for (int i = 0; i < argc; i++) {
+        std::println("Argument {}: {}", i, argv[i]);
+    }
+
     return 0;
 }
