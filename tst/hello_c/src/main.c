ac0e9eb0a99205e24d062669bf8451842fcb29ba
q4niel <git.unrigged621@passmail.com>
2026-09-02 19:59:56 +0200

    add windows entry point and builders to hello_c test

--- tst/hello_c/src/main.c
+++ tst/hello_c/src/main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
 #ifdef DEBUG
-    #define PRINT(...) printf(__VA_ARGS__);
+    #define DPRINT(...) printf(__VA_ARGS__);
 #else
-    #define PRINT(...)
+    #define DPRINT(...)
 #endif
 
-int main(int argc, char **argv) {
-    PRINT("hello c world\n\n")
+#ifdef PLATFORM_LINUX
+    int main(int argc, char **argv)
+#elif PLATFORM_WINDOWS
+    #include <windows.h>
+    #define argc __argc
+    #define argv __argv
+    int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
+#endif
+{
+    DPRINT("hello c world\n\n")
 
-    for (int i = 0; i < argc; i++) {
+    for (int i = 0; i < argc; i++)
         printf("Argument %i: %s\n", i, argv[i]);
-    }
 
     return 0;
 }
