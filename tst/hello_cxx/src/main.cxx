2a9971c2db1327f5f780593add5d95fd50b40123
q4niel <git.unrigged621@passmail.com>
2026-09-05 23:01:34 +0200

    add windows entry point and builders to hello_cxx test

--- tst/hello_cxx/src/main.cxx
+++ tst/hello_cxx/src/main.cxx
@@ -7,13 +7,20 @@ inline constexpr bool kDebug =
         false;
 #endif
 
-auto main(int argc, char **argv) -> int {
+#ifdef PLATFORM_LINUX
+    auto main(int argc, char **argv) -> int
+#elif PLATFORM_WINDOWS
+    #include <windows.h>
+    #define argc __argc
+    #define argv __argv
+    auto WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) -> int
+#endif
+{
     if constexpr (kDebug)
         std::println("hello c++ world\n");
 
-    for (int i = 0; i < argc; i++) {
+    for (int i = 0; i < argc; i++)
         std::println("Argument {}: {}", i, argv[i]);
-    }
 
     return 0;
 }
